18d776735c4429cf5fd9b71e10bf2eb544561a83
qiulanbook <2549474324@qq.com>
2019-06-19 16:41:33 +0800

    t2

--- shengjiang/js/pc_js1.js
+++ shengjiang/js/pc_js1.js
@@ -2,7 +2,7 @@
 * @Author: Administrator
 * @Date:   2019-06-17 15:18:11
 * @Last Modified by:   Administrator
-* @Last Modified time: 2019-06-17 16:03:10
+* @Last Modified time: 2019-06-19 15:33:43
 */
 
 'use strict';
@@ -34,5 +34,38 @@ $(function(){
 		$('.btm ul').eq($(this).index()).show();
 
 	})
+
+
+	// $(function(){
+ //        //需要固定的元素
+ //        var title_section = $('.nav');
+ //        //需要固定的元素距离顶部的大小
+ //        var title_top = title_section.offset().top;
+ //        $(window).scroll(function(){
+ //            //如果滚动的距离大于 title_top ，则固定布局，否则取消固定
+ //            if( $(window).scrollTop() > title_top ){
+ //                title_section.addClass('fixed_top');
+ //            }else{
+ //                title_section.removeClass('fixed_top');
+ //            }
+ //        })
+ //    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 })
 
